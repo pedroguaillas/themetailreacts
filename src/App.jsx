90715c05144be56beb48f8e753b3bfd18df0dbcd
pedroguaillas <peter.tufi@gmail.com>
2023-07-03 12:53:54 -0500

    Start project version 1.0.0

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+function App () {
+  return (
+    <>
+      <p className='text-sky-500'>Hola mundo</p>
+    </>
+  )
+}
+
+export default App
