f8e3c9ba61911437d8dc77a10267c8b14aa68d46
Angel Ortega <angelb.ortega08@gmail.com>
2023-07-06 00:22:47 -0500

    SEgundo

--- src/components/Layout/SeccionDesarrollo.jsx
+++ src/components/Layout/SeccionDesarrollo.jsx
@@ -3,6 +3,7 @@ const SeccionDesarrollo = () => {
     <>
       <div className=" grid sm:col-span-2 lg:col-span-1 text-center">
         <h2 className=" text-blue-950 font-bold text-lg">PANEL</h2>
+        <h3>Este es una Prueba</h3>
         <p>
           Lorem, ipsum dolor sit amet consectetur adipisicing elit. Numquam,
           distinctio, aliquam rem molestias quae explicabo corrupti tempora sint
